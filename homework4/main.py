90c0a84a432b222601e7b268550cc4748faa044a
3120005017何志铜 <hzt3016@gmail.com>
2022-09-30 01:42:17 +0800

    完成凯撒加密和解密功能

--- homework4/main.py
+++ homework4/main.py
@@ -2,8 +2,8 @@ from utils import DataOP, modules
 
 if __name__ == '__main__' :
     data = DataOP.load_data.load_txt()
-    key = 'jshzt'
-    data = modules.XOR_code().Encryption(data, key)
+    key = 3
+    data = modules.Caesar_code().Encryption(data, key)
     print(data)
-    data = modules.XOR_code().Decrypt(data, key)
+    data = modules.Caesar_code().Decrypt(data, key)
     print(data)
--- homework4/utils/modules.py
+++ homework4/utils/modules.py
@@ -49,20 +49,55 @@ class XOR_code(object):
         decry_int = int(translate_string) ^ key_use
         decry_str = self.int2str(decry_int)
         return decry_str
+    
+class Caesar_code(object):   
+    def __init__(self):
+        self.letterSList = ('a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z').split(',')
+        self.letterBList = ('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z').split(',')
 
-class user_info(object):
-    def __init__(self, src, code=None) -> None:
-        self.src = src
-        self.code = code
-    
-class Caesar_code(object):
-    def __init__(self, user:user_info) -> None:
-        self.user = user_info
-    
-    # encode   
-    def CaesarEncryption(self,cipher,shifting): 
-        pass
-    
-    # decode 
-    def CaesarDecrypt(self,cipher,shifting): 
-        pass
+    def Encryption(self, data:list, key:int)->list:
+        result = []
+        for i in data:
+            result.append(self.encryption_line(i, key))
+        return result
+
+    def Decrypt(self, data:list, key:list)->list:
+        result = []
+        for i in data:
+            result.append(self.decryption_line(i, key))
+        return result
+    def encryption_line(self, string:str, key:int):    
+        encryption =''
+        for c in string:
+            if c == ' ' or c =='_' or c=='＿':
+                encryption += c
+            else:
+                if re.match(r'[a-z]', c) != None:
+                    stringNum = self.letterSList.index(c)
+                    stringShiftingNum = (stringNum + key) % 26
+                    encryption += self.letterSList[stringShiftingNum]
+                elif re.match(r'[A-Z]', c) != None:
+                    stringNum = self.letterBList.index(c)
+                    stringShiftingNum = (stringNum + key) % 26
+                    encryption += self.letterBList[stringShiftingNum]
+                else:
+                    return None
+        return encryption 
+
+    def decryption_line(self,string:str , key:int):    
+        decrypt=''
+        for c in string:
+            if c ==' ' or c=='_' or c=='＿':
+                decrypt += c
+            else:
+                if re.match(r'[a-z]', c) != None:
+                    stringNum = self.letterSList.index(c)
+                    stringShiftingNum = (stringNum - key) % 26
+                    decrypt +=self.letterSList[stringShiftingNum]
+                elif re.match(r'[A-Z]', c) != None:
+                    stringNum = self.letterBList.index(c)
+                    stringShiftingNum = (stringNum - key) % 26
+                    decrypt +=self.letterBList[stringShiftingNum]
+                else:
+                    return None
+        return decrypt 
