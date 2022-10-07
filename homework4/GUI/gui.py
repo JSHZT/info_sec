03058770e5ac22d51bd42746c9d5ce0728fd3c24
3120005017何志铜 <hzt3016@gmail.com>
2022-10-07 22:30:47 +0800

    设计完界面

--- homework4/GUI/gui.py
+++ homework4/GUI/gui.py
@@ -1,1 +1,15 @@
 import PyQt5 as qt
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from mainwin import Ui_MainWindow
+
+class MyMainForm(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainForm, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWin = MyMainForm()
+    myWin.show()
+    sys.exit(app.exec_())
---
+++ homework4/rungui.py
@@ -0,0 +1,4 @@
+from GUI import gui
+
+if __name__ == "__main__":
+    pass
