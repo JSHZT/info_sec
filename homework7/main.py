452c8ba744733c45f988e8ae54c7fc839abca9ac
3120005017何志铜 <hzt3016@gmail.com>
2022-10-23 17:07:50 +0800

    完成第二题

--- homework7/main.py
+++ homework7/main.py
@@ -8,7 +8,16 @@ def test1():
     d = rsa_tool().match_d(e, fn)    
     print(d)
 
+def test2():
+    e = 5
+    n = 35
+    C = 10
+    factors = factor(n)
+    fn = (factors[0] - 1)*(factors[1] - 1)
+    d = rsa_tool().match_d(e, fn)    
+    result = rsa_tool().decrypt(d=d, C=C, m=n)
+    print(result)
 
 if __name__ == "__main__":
     test1()    #答案3031
-    
+    test2()    #答案 5
