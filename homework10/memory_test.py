b3eb9c818f93cd639ba72c9f0b6aa2b3c540bf55
3120005017何志铜 <hzt3016@gmail.com>
2022-12-21 21:03:04 +0800

    测试脚本撰写

--- homework10/memory_test.py
+++ homework10/memory_test.py
@@ -9,4 +9,16 @@ def count(num):
         time.sleep(1)
     print("\rtime up！")
 
-
+if __name__ == "__main__":
+    num = 10
+    number = random.randint(100000000, 1000000000)
+    hash_value = hashlib.sha256(str(number).encode()).hexdigest()
+    print("Please memorize the number within {} seconds:".format(num), number)
+    count(num)
+    os.system('cls')
+    answer = input("What was the number? ")
+    hash_answer = hashlib.sha256(str(answer).encode()).hexdigest()
+    if hash_answer == hash_value:
+        print("Correct!")
+    else:
+        print("Incorrect. The correct answer was", number)
