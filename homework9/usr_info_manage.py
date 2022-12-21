efef15fb6949dd69639d234d52e4b9ab8c396668
3120005017何志铜 <hzt3016@gmail.com>
2022-12-21 17:58:25 +0800

    撰写测试脚本

--- homework9/usr_info_manage.py
+++ homework9/usr_info_manage.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import sys
 import json
+import random
+import string
 
 class usr_info_manage(object):
     def __init__(self, args) -> None:
@@ -16,7 +18,7 @@ class usr_info_manage(object):
 
     def register(self, usr, passw)->bool:
         try:
-            salt = os.urandom(8)
+            salt = ''.join(random.sample(string.ascii_letters + string.digits, 8))
             token = self.generate_token(passw, salt)
             self.sql[usr] = (token, salt)
             self.save()
@@ -33,26 +35,39 @@ class usr_info_manage(object):
         return token1 == token2
 
     def save(self):
-        js = json.dumps(self.sql)   
-        file = open('data.txt', 'w')  
+        js = json.dumps(self.sql)
+        file = open(self.args.path_to_data, 'w')  
         file.write(js)  
         file.close()
         
     def load(self):
-        file = open(self.args.path_to_data, 'r') 
+        file = open(self.args.path_to_data, 'r')
         js = file.read()
-        self.sql = json.loads(js)   
+        self.sql = json.loads(js)
         file.close()
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # add the input folder arg 
-    parser.add_argument('--load', type=bool, default=False, help="if true, load the user data from file")
+    parser.add_argument('--load', type=bool, default=True, help="if true, load the user data from file")
     parser.add_argument('--path_to_data', type=str, default=os.path.dirname(__file__)+"/data.txt")
     args = parser.parse_args()
     manager = usr_info_manage(args)
-    
+
     usr = "root"
     passw = "ajzhyadsa"
+
+    if manager.register(usr=usr, passw=passw):
+        print("注册成功！")
+    else:
+        print("注册失败！")
     
-    usr_info_manage.register(usr=usr, passw=passw)
+    fake_passw = "esafdasf"
+    if manager.login(usr=usr, passw=passw):
+        print("登录成功！")
+    else:
+        print("登录失败！")
+    if manager.login(usr=usr, passw=fake_passw):
+        print("登录成功！")
+    else:
+        print("登录失败！")
